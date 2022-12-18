b0d29a655a5c73e4cab5cfd2080c7b9b78bd4d2e
Nevah5 <neevaah5@gmail.com>
2022-12-19 00:22:55 +0100

    Added prefix to config

--- src/main/java/dev/nevah5/mc/lobbyplugin/LobbyConfig.java
+++ src/main/java/dev/nevah5/mc/lobbyplugin/LobbyConfig.java
@@ -1,10 +1,13 @@
 package dev.nevah5.mc.lobbyplugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LobbyConfig {
+    public final String PREFIX = ChatColor.DARK_GRAY + "| " + ChatColor.AQUA + "Server " + ChatColor.DARK_GRAY +
+        ">> "+ChatColor.LIGHT_PURPLE;
     public List<Player> playersEnableBuilding = new ArrayList<>();
 }
