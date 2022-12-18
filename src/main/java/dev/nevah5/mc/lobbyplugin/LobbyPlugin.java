f10a74c43cad26c313f5d97ffc9c1dca32ad6d2a
Nevah5 <neevaah5@gmail.com>
2022-12-18 23:48:44 +0100

    Prevent block breaking

---
+++ src/main/java/dev/nevah5/mc/lobbyplugin/LobbyPlugin.java
@@ -0,0 +1,21 @@
+package dev.nevah5.mc.lobbyplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LobbyPlugin extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onBuild(BlockBreakEvent blockBreakEvent){
+        blockBreakEvent.setCancelled(true);
+    }
+}
--- src/main/java/dev/nevah5/mc/lobbyplugin/Main.java
+++
@@ -1,7 +0,0 @@
-package dev.nevah5.mc.lobbyplugin;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
