ff994eb325c95919fa37bedc1b8de27f41585c58
Nevah5 <neevaah5@gmail.com>
2022-12-26 12:08:04 +0100

    Sort teams in Tab list

--- src/main/java/dev/nevah5/mc/lobbyplugin/tools/TeamTool.java
+++ src/main/java/dev/nevah5/mc/lobbyplugin/tools/TeamTool.java
@@ -29,28 +29,28 @@ public class TeamTool {
         ScoreboardManager scoreboardManager = Bukkit.getScoreboardManager();
         assert scoreboardManager != null;
         Scoreboard scoreboard = scoreboardManager.getMainScoreboard();
-        if(scoreboard.getTeam("group_owner") == null) {
-            this.group_owner = scoreboard.registerNewTeam("group_owner");
+        if(scoreboard.getTeam("A_group_owner") == null) {
+            this.group_owner = scoreboard.registerNewTeam("A_group_owner");
             this.group_owner.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
             this.group_owner.setPrefix(this.lobbyConfig.getConfigurationString("OwnerPrefix"));
         }
-        if(scoreboard.getTeam("group_admin") == null) {
-            this.group_admin = scoreboard.registerNewTeam("group_admin");
+        if(scoreboard.getTeam("B_group_admin") == null) {
+            this.group_admin = scoreboard.registerNewTeam("B_group_admin");
             this.group_admin.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
             this.group_admin.setPrefix(this.lobbyConfig.getConfigurationString("AdminPrefix"));
         }
-        if(scoreboard.getTeam("group_mod") == null){
-            this.group_mod = scoreboard.registerNewTeam("group_mod");
+        if(scoreboard.getTeam("C_group_mod") == null){
+            this.group_mod = scoreboard.registerNewTeam("C_group_mod");
             this.group_mod.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
             this.group_mod.setPrefix(this.lobbyConfig.getConfigurationString("ModPrefix"));
         }
-        if(scoreboard.getTeam("group_friend") == null){
-            this.group_friend = scoreboard.registerNewTeam("group_friend");
+        if(scoreboard.getTeam("V_group_friend") == null){
+            this.group_friend = scoreboard.registerNewTeam("V_group_friend");
             this.group_friend.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
             this.group_friend.setPrefix(this.lobbyConfig.getConfigurationString("FriendPrefix"));
         }
-        if(scoreboard.getTeam("group_default") == null) {
-            this.group_default = scoreboard.registerNewTeam("group_default");
+        if(scoreboard.getTeam("Z_group_default") == null) {
+            this.group_default = scoreboard.registerNewTeam("Z_group_default");
             this.group_default.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
             this.group_default.setPrefix(this.lobbyConfig.getConfigurationString("DefaultPrefix"));
         }
